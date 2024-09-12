6ae6f9c965451d8b478baabad1d91fd929451cdf
bhargav-tibadiya <bhargavptibadiya.tbp@gmail.com>
2024-09-12 17:51:37 +0530

    login page design completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,37 @@
-import { NavLink, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { FaSearch, FaAngleDown } from "react-icons/fa";
 import styles from "./Navbar.module.scss"
 import { routes } from "../../config/routes";
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [selectedMenu, SetSelectedMenu] = useState(0)
+
+  console.log('selectedMenu', selectedMenu)
+
   const navigate = useNavigate();
+
   return (
-    <nav className={styles.navbar}>
+    <div className={styles.navbar}>
+
       <div className={styles.website_logo}>
         <span className={styles.circle_logo}>C</span>
         <span>CrowdEstate</span>
       </div>
-      <div className={styles.website_section}>
-        <ul>
-          <li><NavLink to={routes.home}>Home</NavLink></li>
-          <li><NavLink to={routes.app}>App</NavLink><FaAngleDown /></li>
-          <li><NavLink to={routes.about}>About us</NavLink></li>
-          <li><NavLink to={routes.contact}>Contact us</NavLink></li>
-        </ul>
 
+      <div className={styles.website_section}>
+        <div className={`${styles.nav_items} ${selectedMenu == 0 ? styles.active : ""}`} onClick={() => { navigate(routes.home), SetSelectedMenu(0) }}>Home</div>
+        <div className={`${styles.nav_items} ${selectedMenu == 1 ? styles.active : ""}`} onClick={() => { navigate(routes.app), SetSelectedMenu(1) }}> <span>App </span><FaAngleDown /></div>
+        <div className={`${styles.nav_items} ${selectedMenu == 2 ? styles.active : ""}`} onClick={() => { navigate(routes.about), SetSelectedMenu(2) }}>About us</div>
+        <div className={`${styles.nav_items} ${selectedMenu == 3 ? styles.active : ""}`} onClick={() => { navigate(routes.contact), SetSelectedMenu(3) }}>Contact us</div>
       </div>
+
       <div className={styles.website_other_icon}>
         <div className={styles.search_icon}><FaSearch /></div>
         <button onClick={() => navigate(routes.login)}>Sign up</button>
       </div>
-    </nav>
+    </div>
   )
 }
 
