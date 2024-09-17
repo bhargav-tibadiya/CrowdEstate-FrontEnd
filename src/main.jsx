39f58e499470edf94f32a239c3ac257695d046df
bhargav-tibadiya <bhargavptibadiya.tbp@gmail.com>
2024-09-17 17:00:18 +0530

    vecel analytics and test added

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import './index.scss'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={googleClientId}>
       <BrowserRouter>
+        <Analytics />
+        <SpeedInsights />
         <App />
       </BrowserRouter>
     </GoogleOAuthProvider>
