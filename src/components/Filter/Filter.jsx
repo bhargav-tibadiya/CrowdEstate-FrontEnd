cead0f7a6998c2c7d0659877168102bfa36f5350
priya <priyankbhalala1911@gmail.com>
2024-10-12 18:47:44 +0530

    update filter and add tags field in fileter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,7 +3,7 @@ import styles from "./Filter.module.scss"
 
 const Filter = ({ onFilterChange }) => {
     const [location, setLocation] = useState('');
-    const [estateType, setEstateType] = useState('');
+    const [tags, setTags] = useState([]);
     const [priceRange, setPriceRange] = useState('');
     const [beds, setBeds] = useState('');
     const [sqft, setSqft] = useState('');
@@ -12,13 +12,18 @@ const Filter = ({ onFilterChange }) => {
     useEffect(() => {
         onFilterChange({
             location,
-            estateType,
+            tags,
             priceRange,
             beds,
             sqft,
             category,
         });
-    }, [location, estateType, priceRange, beds, sqft, category])
+    }, [location, tags, priceRange, beds, sqft, category])
+
+    const handleMultiSelectChange = (event, setStateFunction) => {
+        const selectedOptions = Array.from(event.target.selectedOptions, option => option.value);
+        setStateFunction(selectedOptions);
+      };
 
     return (
         <div className={styles.filter_container}>
@@ -43,17 +48,29 @@ const Filter = ({ onFilterChange }) => {
 
             <div className={styles.input_fields}>
                 <div className={styles.fields}>
-                    <label htmlFor="type">Type</label>
+                    <label htmlFor="tags">Tags</label>
                     <select
-                        id="type"
-                        name="type"
-                        value={estateType}
-                        onChange={(e) => setEstateType(e.target.value)}
+                        id="tags"
+                        name="tags"
+                        value={tags}
+                        onChange={(e) => handleMultiSelectChange(e, setTags)}
                     >
-                        <option value="">Select type</option>
-                        <option value="house">House</option>
-                        <option value="apartment">Apartment</option>
-                        <option value="condo">Condo</option>
+                        <option value="">Select Tags</option>
+                        <option value="Luxury">Luxury</option>
+                        <option value="Affordable">Affordable</option>
+                        <option value="Under Construction">Under Construction</option>
+                        <option value="Fully Furnished">Fully Furnished</option>
+                        <option value="Pet Friendly">Pet Friendly</option>
+                        <option value="Near School">Near School</option>
+                        <option value="Near Metro">Near Metro</option>
+                        <option value="Gated Community">Gated Community</option>
+                        <option value="Eco-Friendly">Eco-Friendly</option>
+                        <option value="Smart Home">Smart Home</option>
+                        <option value="Waterfront">Waterfront</option>
+                        <option value="Mountain View">Mountain View</option>
+                        <option value="City Center">City Center</option>
+                        <option value="Garden">Garden</option>
+                        <option value="Swimming Pool">Swimming Pool</option>
                     </select>
                 </div>
             </div>
@@ -68,7 +85,7 @@ const Filter = ({ onFilterChange }) => {
                         value={priceRange}
                         onChange={(e) => setPriceRange(e.target.value)}
                     >
-
+                        <option value="">Select Price Range</option>
                         <option value="0-100000">Under ₹100,000</option>
                         <option value="100000-300000">₹100,000 - ₹300,000</option>
                         <option value="300000-500000">₹300,000 - ₹500,000</option>
@@ -89,10 +106,11 @@ const Filter = ({ onFilterChange }) => {
                         onChange={(e) => setBeds(e.target.value)}
                     >
                         <option value="">Beds</option>
-                        <option value="1">1+</option>
-                        <option value="2">2+</option>
-                        <option value="3">3+</option>
-                        <option value="4">4+</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5+</option>
                     </select>
                 </div>
             </div>
@@ -107,19 +125,20 @@ const Filter = ({ onFilterChange }) => {
                         value={sqft}
                         onChange={(e) => setSqft(e.target.value)}
                     >
+                        <option value="">Select Size</option>
                         <option value="0-1000">under 1000ft</option>
                         <option value="1000-1500">1000ft - 1500ft</option>
                         <option value="1500-2000">1500ft - 2000ft</option>
                         <option value="2000-2500">2000ft - 2500ft</option>
                         <option value="2500-3000">2500ft - 3000ft</option>
-                        <option value="3000-">Over 3000ft</option>
+                        <option value="3000">Over 3000ft</option>
                     </select>
                 </div>
             </div>
 
             {/* Sqft Range */}
             <div className={styles.input_fields}>
-                <div className={styles.fields}>
+                <div className={styles.fields}> 
                     <label htmlFor="Category">Category</label>
                     <select
                         id="Category"
--- src/pages/Market/Market.jsx
+++ src/pages/Market/Market.jsx
@@ -1,5 +1,6 @@
 // Importing hooks
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 // Importing Components and Styling
@@ -10,8 +11,9 @@ import { fetchAllProperties } from '../../services/propertyApi'
 import Filter from '../../components/Filter/Filter'
 
 
-
 const Market = () => {
+
+  const navigate = useNavigate()
 
   const [data, setData] = useState([]);
   const [filteredProperties, setFilteredProperties] = useState([]);
@@ -23,6 +25,7 @@ const Market = () => {
 
         const response = await fetchAllProperties();
         setData(response.properties);
+        setFilteredProperties(response.properties)
 
       } catch (error) {
 
@@ -36,10 +39,11 @@ const Market = () => {
   }, [])
 
 
- 
+
 
   const handleFilterChange = (filters) => {
-    const { location, priceRange, beds, sqft, category } = filters;
+    const { location, tags, priceRange, beds, sqft, category } = filters;
+    console.log(tags)
 
     // Handle empty or undefined priceRange and sqftRange
     const [minPrice, maxPrice] = priceRange ? priceRange.split('-').map(Number) : [0, Infinity];
@@ -47,16 +51,21 @@ const Market = () => {
 
     const filtered = data.filter((property) => {
 
+      const hasTags = tags.every((tag) => property.tags.includes(tag));
+      console.log(hasTags)
       const propertyLocation = `${property.location.city} ${property.location.state} ${property.location.country}`.toLowerCase();
       const propertyPrice = property.price;
       const propertySqft = property.size;
       const propertyBeds = property.bedrooms;
       const propertyCategory = property.category;
 
+
+
       return (
         (!location || propertyLocation.includes(location.toLowerCase())) &&
-        (!priceRange || (propertyPrice >= (minPrice || 0) && propertyPrice <= (maxPrice || Infinity))) &&
-        (!beds || propertyBeds == beds) &&
+        (!tags.length || hasTags) &&
+        (!priceRange || (propertyPrice >= (minPrice || 0) && propertyPrice <= (maxPrice || Infinity) && propertyPrice > 3000)) &&
+        (!beds || (beds == 5 ? propertyBeds >= 5 : propertyBeds == beds)) &&
         (!sqft || (propertySqft >= (minSqft || 0) && propertySqft <= (maxSqft || Infinity))) &&
         (!category || propertyCategory == category)
       );
@@ -81,12 +90,13 @@ const Market = () => {
         <div className={styles.filter_container}>
           <Filter onFilterChange={handleFilterChange} />
         </div>
+        {
+          filteredProperties.length === 0 && (<div className={styles.not_property}>No Properties found</div>)
+        }
 
         <div className={styles.property_container}>
-
           {
-            
-            (filteredProperties.length > 0 ? filteredProperties:data).map((item, index) => {
+            filteredProperties.length > 0 && (filteredProperties.map((item, index) => {
               return (
                 <div key={index} className={styles.property}>
                   <div className={styles.image}>
@@ -97,34 +107,14 @@ const Market = () => {
                       <span className={styles.price}>{item.price}</span>
                       <span className={styles.name}>{item.name}, {item.city}</span>
                     </div>
-                    <div onClick={() => { }} className={styles.button}>
+                    <div onClick={() => { navigate(`/property/${item._id}`) }} className={styles.button}>
                       <FaCaretRight />
                     </div>
                   </div>
                 </div>
               )
-            })
+            }))
           }
-
-          {/* //! Delete this */}
-
-          <div className={styles.property}>
-            <div className={styles.image}>
-              <img src="https://res.cloudinary.com/bhargavspace/image/upload/v1728225132/CrowdEstate/ta9shqclrvj0zw59zrx4.jpg" alt="" />
-            </div>
-            <div className={styles.description}>
-              <div className={styles.info}>
-                <span className={styles.price}>12,000,000</span>
-                <span className={styles.name}>Greenfield Colony, Bengaluru</span>
-              </div>
-              <div onClick={() => { }} className={styles.button}>
-                <FaCaretRight />
-              </div>
-            </div>
-          </div>
-
-
-
         </div>
       </div>
     </div>
